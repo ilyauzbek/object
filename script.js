c2741a4a4478ce5fc4e9afedea867b5f8d02a4e9
ilyauzbek <Stas.yun.82@gmail.com>
2024-09-10 11:48:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let a = {1: 'понедельник' , 2: 'Вторник' , 3: 'Среда' , 4: 'Четверг' , 5: 'Пятница' , 6: 'Суббота' , 7: 'Воскресенье'}
+
+console.log(a)
+
+let b = {1: 'Январь' , 2: 'Февраль' , 3: 'Март' , 4: 'Апрель' , 5: 'Май' , 6: 'Июнь' , 7: 'Июль' , 8: 'Август' , 9: 'Сентябрь' , 10: 'Октябарь' , 11: 'Ноябрь' , 12: 'Декабрь' ,}
+
+console.log(b) 
+
+let user = {Name : 'Ilya' , surname : 'Yun' , patronymic :'Stanislavovich'};
+
+console.log(user)
+
+let date = {year: '2010', month: 'February', day: '2'}
+
+console.log(date)
+
+let d ={a: '11'}
+
+d['a'] = '12'
+
+console.log(d)
+
+let obj = {x: 1, y: 2, z: 3};
+let key = 'x';
+
+console.log(obj[key]);
+
+
+
